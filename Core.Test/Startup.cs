4528c3735a855d8e46ad2185a0a3d8981ac5c949
Reid Peryam <TheFastCat@hotmail.com>
2014-06-30 16:01:45 -0600

    Skinnifying

---
+++ Core.Test/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace Core
+{
+    using Microsoft.Owin.Extensions;
+    using Microsoft.Owin.FileSystems;
+    using Microsoft.Owin.StaticFiles;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileSystem = new EmbeddedResourceFileSystem(typeof(UI.Hooker).Assembly, "UI")
+            });
+            app.UseStageMarker(PipelineStage.MapHandler); // http://goo.gl/XrYGMh
+        }
+    }
+}
